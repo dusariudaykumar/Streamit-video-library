3ad5a5b6864160d7bffc46ce9a0f80adb5534e57
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-28 22:49:13 +0530

    feat: added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { NavBar } from "./components";
+import { useLocation } from "react-router-dom";
+import { NavBar, SideNav } from "./components";
+import { RoutesPath } from "./Routes/RoutesPath";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <div className="main-wrapper">
-        <NavBar />
+        {location.pathname !== "/login" && location.pathname !== "/signup" && (
+          <NavBar />
+        )}
+        {location.pathname !== "/login" && location.pathname !== "/signup" && (
+          <SideNav />
+        )}
+        <RoutesPath />
       </div>
     </div>
   );
