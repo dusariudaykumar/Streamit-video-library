73feecba4627f79363a38dee04ed8c2e5464fe94
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-30 23:15:00 +0530

    added : authcontext

---
+++ src/Contexts/auth-context.js
@@ -0,0 +1,23 @@
+import { createContext, useReducer, useContext } from "react";
+import { authReducer } from "../Reducers";
+
+const initialSate = {
+  isAuth: localStorage.getItem("token") ? true : false,
+  encodedToken: localStorage.getItem("token") || "",
+  userData: JSON.parse(localStorage.getItem("userData")) || "",
+};
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [authState, authDispatch] = useReducer(authReducer, initialSate);
+  return (
+    <AuthContext.Provider value={{ authState, authDispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
