6479bb27aeb2a7ee326a9860191d836ab48dacce
Uday Kumar <dudaykumargoud@gmail.com>
2022-05-03 12:58:34 +0530

    feat: listing videos

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,18 @@
 import { VideoCard } from "../../components";
+import { useVideo } from "../../Contexts";
 import "./HomePage.css";
 const HomePage = () => {
+  const {
+    videoState: { videos },
+    videoDispatch,
+  } = useVideo();
   return (
     <div className="home-page-wrapper">
-      <VideoCard />
+      <div className="videos-wrapper">
+        {videos.map((video) => {
+          return <VideoCard key={video._id} video={video} />;
+        })}
+      </div>
     </div>
   );
 };
