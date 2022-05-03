6b03048616f8449587640191a0f0f31a99fdc9bb
Uday Kumar <dudaykumargoud@gmail.com>
2022-05-03 12:58:00 +0530

    updated: page routes

--- src/Routes/RoutesPath.jsx
+++ src/Routes/RoutesPath.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, Login, Signup } from "../Pages";
+import { HomePage, Login, Signup, SinglePageVideo } from "../Pages";
 
 const RoutesPath = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/video/:videoId" element={<SinglePageVideo />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
