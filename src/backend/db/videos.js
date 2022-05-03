1951d4bc83ad3ec8d28ad44f66c0d1af3e0759c5
Uday Kumar <dudaykumargoud@gmail.com>
2022-05-03 13:01:40 +0530

    updated :  backend data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -8,24 +8,33 @@ export const videos = [
     _id: "6Xwcspy3Cdk",
     title: "Apex Legends - Saviors Launch Trailer | PS4 Games",
     creator: "PlayStation",
-    description: "",
+    description:
+      "When a monstrous beast attacks Storm Point, new Legend Newcastle leaps into the fray to protect and inspire his fellow Legends: because that’s what heroes are for. ",
     views: "14K",
+    published: "Apr 27, 2022",
+    duration: "3:54",
     categoryName: "Trailer",
   },
   {
     _id: "tV95N0TIltc",
     title: "Grand Theft Auto V and GTA Online - Launch Trailer | PS5",
     creator: "PlayStation",
-    description: "",
+    description:
+      "Experience blockbuster hits Grand Theft Auto V and GTA Online – Out now on PlayStation 5 with stunning visuals, faster loading, adaptive triggers, haptic feedback, Tempest 3D audio and more. ",
     views: "292K",
+    duration: "1:04",
+    published: "Mar 15, 2022",
     categoryName: "Trailer",
   },
   {
     _id: "d5nfXqffvyc",
     title: "UNCHARTED 4: A Thief's End - Madagascar Preview | PS4",
     creator: "PlayStation",
-    description: "",
+    description:
+      "See Nathan, Sam, and Sully explore the plains of Madagascar in this gameplay video.",
     views: "10M",
+    duration: "15:54",
+    published: "Apr 4, 2016",
     categoryName: "PS4 Games",
   },
   {
@@ -33,16 +42,22 @@ export const videos = [
     title:
       "God Of War Ragnarok - PlayStation Showcase 2021 Reveal Trailer | PS5",
     creator: "PlayStation",
-    description: "",
+    description:
+      "From Santa Monica Studio comes the sequel to the critically acclaimed God of War (2018).",
     views: "12M",
+    duration: "3:17",
+    published: "Sep 10, 2021",
     categoryName: "Trailer",
   },
   {
     _id: "0Mlv72oV1y4",
     title: "Ultimate PlayStation 5 Gaming Setup Guide",
     creator: "Digital Trends",
-    description: "",
+    description:
+      "Is the Sony PlayStation 5 and Sony X900H 4K LED TV a match made in gaming heaven? We ran into a few surprises as we paired the two to see how well the two work together, what settings should be adjusted on the TV and the console, how good the graphics look, and what, if any, limitations may be in play.",
     views: "1.2M",
+    duration: "12:35",
+    published: "Nov 23, 2020",
     categoryName: "Setup Guide",
   },
 
@@ -50,8 +65,11 @@ export const videos = [
     _id: "zrROdKbBX3k",
     title: "Marvel's Avengers PS5 - Iron Man Gameplay (Avengers: Endgame Suit)",
     creator: "ynSection",
-    description: "",
+    description:
+      "Check out some gameplay of Marvel Studios' Avengers: Endgame outfit for Iron Man, featuring the Armor Chase Mission on the PlayStation 5.",
     views: "4M",
+    duration: "4:45",
+    published: "Aug 3, 2021",
     categoryName: "Gameplay",
   },
 
@@ -59,8 +77,11 @@ export const videos = [
     _id: "wQATS4HOxdo",
     title: "Horizon Forbidden West - State of Play Gameplay Reveal | PS5",
     creator: "Play station",
-    description: "",
+    description:
+      "Join Aloy as she braves the Forbidden West, a deadly frontier that conceals mysterious new ",
     views: "9.9M",
+    duration: "19:11",
+    published: "May 28, 2021",
     categoryName: "Gameplay",
   },
 
@@ -69,8 +90,11 @@ export const videos = [
     title:
       "FIFA 22 - Arsenal vs. Man United - Premier League Full Match PS5 Gameplay | 4K",
     creator: "MJ7",
-    description: "",
+    description:
+      "FIFA 22 PS5 Gameplay in 4K. Arsenal vs. Manchester United - Premier League Full Match at Emirates Stadium.",
     views: "25k",
+    duration: "23:34",
+    published: "",
     categoryName: "Gameplay",
   },
 ];
