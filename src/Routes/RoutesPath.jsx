d24c98f8c32f406731537a1155bd4a89da7bf784
Uday Kumar <dudaykumargoud@gmail.com>
2022-05-04 11:38:02 +0530

    added : Mockman route and style link

--- src/Routes/RoutesPath.jsx
+++ src/Routes/RoutesPath.jsx
@@ -1,5 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, Login, Signup, SinglePageVideo } from "../Pages";
+import Mockman from "mockman-js";
+import {
+  ExplorePage,
+  HomePage,
+  Login,
+  Signup,
+  SinglePageVideo,
+} from "../Pages";
 
 const RoutesPath = () => {
   return (
@@ -7,8 +14,10 @@ const RoutesPath = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/video/:videoId" element={<SinglePageVideo />} />
+      <Route path="/explore" element={<ExplorePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/mockman" element={<Mockman />} />
     </Routes>
   );
 };
