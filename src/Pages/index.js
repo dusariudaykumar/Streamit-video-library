d24179d64ce0f2627b4def90213231ac15784ff1
Uday Kumar <dudaykumargoud@gmail.com>
2022-05-04 16:20:48 +0530

    update : barrel exports file

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from "./HomePage/HomePage";
 export { Login } from "./AuthPages/Login";
 export { Signup } from "./AuthPages/Signup";
 export { SinglePageVideo } from "./SinglePageVideo/SinglePageVideo";
+export { ExplorePage } from "./ExplorePage/ExplorePage";
