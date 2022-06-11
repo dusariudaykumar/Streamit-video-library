1afd017a81fe8cd2554a8120e20e053377eaf1ca
Uday Kumar <dudaykumargoud@gmail.com>
2022-06-11 16:02:16 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { NavBar } from "./NavBar/NavBar";
 export { SideNav } from "./SideNav/SideNav";
 export { VideoCard } from "./Card/VideoCard";
 export { VerticalVideoCard } from "./VerticalVideoCard/VerticalVideoCard";
+export { PlayListModel } from "./PlayListModel/PlayListModel";
+export { PlayListCard } from "./PlayListCard/PlayListCard";
