5ed02a514a8ea7d384947ae04b8292ffe0527145
Uday Kumar <dudaykumargoud@gmail.com>
2022-06-11 18:49:30 +0530

    Update index.js

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,4 +1,5 @@
 export { addLikeToVideos, removeLikeFromVideo } from "./likesServices";
 export * from "./watchLaterServices";
+export * from "./playlistServices";
 export { getVideosService } from "./getVideosService";
 export { getCategories } from "./getCategories";
