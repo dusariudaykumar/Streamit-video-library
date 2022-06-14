b96799a8bcf4945ce712b86171c80c0cdc1789ad
Uday Kumar <dudaykumargoud@gmail.com>
2022-06-14 20:31:54 +0530

    Update HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,17 +1,24 @@
 import { VideoCard } from "../../components";
 import { useVideo } from "../../Contexts";
+import { searchVideos } from "../../Utils/searchVideo";
 
 import "./HomePage.css";
 const HomePage = () => {
   const {
-    videoState: { videos },
+    videoState: { videos, searchQuerytext },
   } = useVideo();
+  const searchedVideos = searchVideos(searchQuerytext, videos);
+
   return (
     <div className="home-page-wrapper">
       <div className="videos-wrapper flex">
-        {videos.map((video) => {
-          return <VideoCard key={video._id} video={video} />;
-        })}
+        {searchedVideos.length === 0 ? (
+          <p className="p-lg not-found-msg"> No videos found!</p>
+        ) : (
+          searchedVideos.map((video) => {
+            return <VideoCard key={video._id} video={video} />;
+          })
+        )}
       </div>
     </div>
   );
