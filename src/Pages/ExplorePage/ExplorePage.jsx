11c036f51671693f228bb48e6d1ae187c2fea075
Uday Kumar <dudaykumargoud@gmail.com>
2022-06-14 20:32:01 +0530

    Update ExplorePage.jsx

--- src/Pages/ExplorePage/ExplorePage.jsx
+++ src/Pages/ExplorePage/ExplorePage.jsx
@@ -1,14 +1,16 @@
 import { PlayListModel, VideoCard } from "../../components";
 import { useData, useVideo } from "../../Contexts";
 import { filterByCategories } from "../../Utils/filterByCategories";
+import { searchVideos } from "../../Utils/searchVideo";
 import "./ExplorePage.css";
 const ExplorePage = () => {
   const {
     videoDispatch,
-    videoState: { categories, videos, categoryName },
+    videoState: { categories, videos, categoryName, searchQuerytext },
   } = useVideo();
   const { openPlayListModal } = useData();
   const filteredVideos = filterByCategories(categoryName, videos);
+  const searchedVideos = searchVideos(searchQuerytext, filteredVideos);
   return (
     <div>
       <div className="chips-wrapper">
@@ -31,9 +33,13 @@ const ExplorePage = () => {
         })}
       </div>
       <div className="videos-wrapper">
-        {filteredVideos.map((video) => {
-          return <VideoCard key={video._id} video={video} />;
-        })}
+        {searchedVideos.length === 0 ? (
+          <p className="p-lg not-found-msg"> No videos found!</p>
+        ) : (
+          searchedVideos.map((video) => {
+            return <VideoCard key={video._id} video={video} />;
+          })
+        )}
       </div>
       {openPlayListModal && (
         <div className="modal-open">
