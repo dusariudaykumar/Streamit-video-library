8ff0004bfe39acbdce21f8b2341b394c2e11cbdc
Uday Kumar <dudaykumargoud@gmail.com>
2022-06-14 20:28:50 +0530

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,23 +10,43 @@ import {
 import userAvatar from "../../asserts/user.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
-import { useAuth } from "../../Contexts";
+import { useAuth, useData, useVideo } from "../../Contexts";
 import { useState } from "react";
+import { useOnClickOutside } from "../../Utils/onClickOutside";
 const NavBar = () => {
   const [showUserMenu, setShowUserMenu] = useState(false);
+  const { videoDispatch } = useVideo();
+  const outSideClickCloseModelRef = useOnClickOutside(() =>
+    setShowUserMenu(false)
+  );
+
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
+
   const {
     authState: { isAuth, userData },
   } = useAuth();
-
+  const {
+    dataState: { likes },
+    dataDispatch,
+  } = useData();
   const logoutHandler = () => {
     localStorage.clear();
     navigate(0);
   };
+
+  const searchClickHandler = () => {
+    videoDispatch({ type: "SEARCH_QUERY", payload: searchQuery });
+  };
+
   return (
     <nav className="navigation-container flex">
       <div className="nav-left-wrapper flex">
-        <BiMenu className="menu-btn" size="2rem" />
+        <BiMenu
+          className="menu-btn"
+          size="2rem"
+          onClick={() => dataDispatch({ type: "TOGGLE_MENU" })}
+        />
         <div className="logo-container">
           <Link to="/">
             <img className="logo" src={logoImg} alt="logo" />
@@ -34,13 +54,22 @@ const NavBar = () => {
         </div>
       </div>
       <div className="search-container">
-        <input type="search" className="search-feild" placeholder="Search" />
-        <BiSearchAlt2 className="search-icon" size="2rem" />
+        <input
+          type="search"
+          className="search-feild"
+          placeholder="Search"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <BiSearchAlt2
+          className="search-icon"
+          size="2rem"
+          onClick={searchClickHandler}
+        />
       </div>
-      <div className="nav-right-wrapper flex">
+      <div className="nav-right-wrapper flex" ref={outSideClickCloseModelRef}>
         <div className="badge-container">
           <BiBell className="notification-icon" size="2rem" />
-          <span className="icon-badge-count bell-icon">2</span>
+          <span className="icon-badge-count bell-icon">{likes.length}</span>
         </div>
         {isAuth ? (
           <div className="avatar-container">
@@ -61,9 +90,9 @@ const NavBar = () => {
                     <CgProfile className="profile-icon" size="1.6rem" />
                     <span>Profile</span>
                   </li>
-                  <li className="user-menu-item">
+                  <li className="user-menu-item" onClick={logoutHandler}>
                     <BiLogOutCircle className="logout-icon" size="1.6rem" />
-                    <span onClick={logoutHandler}>Logout</span>
+                    <span>Logout</span>
                   </li>
                 </ul>
               </div>
